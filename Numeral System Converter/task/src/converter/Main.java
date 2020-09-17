2c362208f131c184b9c2de2da552fe3c94044dcf
random-jordan <jam35d12@gmail.com>
2020-09-18 04:56:58 +0500

    Done with 3/6

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -7,6 +7,17 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println(scanner.nextInt() % 8);
+        int num = scanner.nextInt();
+        switch (scanner.nextInt()) {
+            case 2:
+                System.out.println("0b" + Integer.toBinaryString(num));
+                break;
+            case 8:
+                System.out.println("0" + Integer.toOctalString(num));
+                break;
+            case 16:
+                System.out.println("0x" + Integer.toHexString(num));
+                break;
+        }
     }
 }
