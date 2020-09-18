456882f38534412a9994baa3f803fd0f6b047893
random-jordan <jam35d12@gmail.com>
2020-09-18 09:42:13 +0500

    Finished 4/6 steps

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -7,17 +7,43 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int num = scanner.nextInt();
-        switch (scanner.nextInt()) {
-            case 2:
-                System.out.println("0b" + Integer.toBinaryString(num));
-                break;
-            case 8:
-                System.out.println("0" + Integer.toOctalString(num));
-                break;
-            case 16:
-                System.out.println("0x" + Integer.toHexString(num));
-                break;
+        int sourceRadix = scanner.nextInt();
+        int num = parse(scanner.next(), sourceRadix);
+        int newBase = scanner.nextInt();
+
+        System.out.println(convert(num, newBase));
+    }
+    private static int parse(String num, int base) {
+        int value = 0;
+        if (base == 1) {
+            for (int i = 0; i < num.length(); i++) {
+                value = num.charAt(i) == '1' ? value + 1 : value;
+            }
+        } else {
+            value = Integer.parseInt(num, base);
         }
+
+        return value;
+    }
+    private static String convert(int num, int newBase) {
+        StringBuilder srt = new StringBuilder();
+        if (newBase == 36) {
+            while (num > 0) {
+                int part = num % 36;
+                if (part < 11) {
+                    srt.insert(0, (char) (part + 48));
+                } else {
+                    srt.insert(0, (char) (part + 87));
+                }
+                num = num / 36;
+            }
+        } else if (newBase == 1) {
+            for (;num > 0; num--) {
+                srt.append("1");
+            }
+        } else {
+            return Integer.toString(num, newBase);
+        }
+        return srt.toString();
     }
 }
